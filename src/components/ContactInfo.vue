df42f6b94873d364ab9d88ce4b0db9404e1cf74b
Chinh Le <chinh.le11@gmail.com>
2019-11-02 10:48:20 -0400

    added contact form

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,5 +1,60 @@
 <template>
   <div class="contactInfo">
-    <h3>Contact Information</h3>
+    <h3>Get in Touch</h3>
+    <form @submit.prevent="onSubmit()">
+      <ul>
+        <li>
+          <input
+            v-model.lazy="user.name"
+            type="text"
+            placeholder="Name"
+            >
+          <input
+            v-model.lazy="user.email"
+            type="email"
+            placeholder="Email"
+            >
+        </li>
+        <li>
+          <input
+            v-model.lazy="user.subject"
+            type="text"
+            placeholder="Subject (optional)"
+            >
+        </li>
+        <li>
+          <textarea
+            id=""
+            v-model.lazy="user.message"
+            name=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+            />
+        </li>
+      </ul>
+      <button type="submit">
+        Send Message
+      </button>
+    </form>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      user: {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+      }
+    };
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.user);
+    }
+  }
+};
+</script>
