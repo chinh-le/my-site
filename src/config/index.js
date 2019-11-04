4f8df44c82bbc317939c4f4755dfe6d5f9c9df5e
Chinh Le <chinh.le11@gmail.com>
2019-11-04 13:15:42 -0500

    added app config

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,12 @@ export default {
   apiKey: process.env.VUE_APP_GOOGLE_API_KEY,
   authDomain: process.env.VUE_APP_GOOGLE_AUTH_DOMAIN,
   databaseUrl: process.env.VUE_APP_GOOGLE_DATABASE_URL,
+  projectId: process.env.VUE_APP_GOOGLE_PROJECT_ID,
   storageBucket: process.env.VUE_APP_GOOGLE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_GOOGLE_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.VUE_APP_GOOGLE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_GOOGLE_APP_ID,
+  measurementId: process.env.VUE_APP_GOOGLE_MEASUREMENT_ID,
+  gmailAccount: process.env.VUE_APP_GMAIL_ACCOUNT,
+  gmailLogin: process.env.VUE_APP_GMAIL_LOGIN,
+  gmailPassword: process.env.VUE_APP_GMAIL_PASSWORD
 };
